9164ea82150f03f301f0c09c4b4631bafcee49d3
shivanimudda <shivani123.xworkz@gmil.com>
2024-07-02 17:28:50 +0530

    2-july-2024

---
+++ Family.java
@@ -0,0 +1,15 @@
+public class Family{
+public static void details(String name){
+System.out.println("family name:"+name);
+
+}
+public static void details(String name,int age){
+System.out.println("family name:"+name+" " +"age:"+age);
+
+}
+public static void details(String name,int age,int siblings){
+System.out.println("family name:"+name+" " +"age:"+age+ " " +"siblings are:"+siblings);
+
+}
+
+}
---
+++ FamilyRunner.java
@@ -0,0 +1,7 @@
+public class FamilyRunner{
+public static void main(String args[]){
+Family.details("father");
+Family.details("mother",34);
+Family.details("sister",34,7);
+}
+}
