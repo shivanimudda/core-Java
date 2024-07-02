e19af33c1b6aa6dbf4b80d797ff8b04a414c6925
shivanimudda <shivani123.xworkz@gmil.com>
2024-07-02 10:09:40 +0530

    first pro

---
+++ AccountRunner.java
@@ -0,0 +1,15 @@
+public class AccountRunner{
+
+	public static void main(String args[]){
+
+		System.out.println("there is account");
+		
+		Account.name();
+		Account.no();
+		Account.customername();
+        Account.amount();
+		Account.balance();
+	}
+
+
+}
