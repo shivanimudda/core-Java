586df8e86d01ec2fa520254d8a578beff34d62cd
shivanimudda <shivani123.xworkz@gmil.com>
2024-07-02 17:39:53 +0530

    2-july-2024

---
+++ Dealer.java
@@ -0,0 +1,15 @@
+public class Dealer{
+public static void details(String name){
+System.out.println("dealer name:"+name);
+
+}
+public static void details(String name,long phno){
+System.out.println("family name:"+name+" " +"phno:"+phno);
+
+}
+public static void details(String name,long phno,String location){
+System.out.println("family name:"+name+" " +"phno:"+phno+ " " +"location are:"+location);
+
+}
+
+}
---
+++ DealerRunner.java
@@ -0,0 +1,7 @@
+public class DealerRunner{
+public static void main(String args[]){
+Dealer.details("suresh");
+Dealer.details("ravi",3467899);
+Dealer.details("suresh",3467990,"Btm");
+}
+}
