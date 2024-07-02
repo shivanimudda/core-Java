03af621aa375abf325573458a74647ce7ed23a37
shivanimudda <shivani123.xworkz@gmil.com>
2024-07-02 17:17:36 +0530

    method 2-july-2024

---
+++ Paint.java
@@ -0,0 +1,10 @@
+public class Paint{
+public static void details(String name){
+System.out.println("paint name:"+name);
+
+}
+public static void details(String name,int price){
+System.out.println("paint name:"+name+" " +"price:"+price);
+
+}
+}
---
+++ PaintRunner.java
@@ -0,0 +1,7 @@
+public class PaintRunner{
+public static void main(String args[]){
+Paint.details("oil");
+Paint.details("asian",34);
+
+}
+}
